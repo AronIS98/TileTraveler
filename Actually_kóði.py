4e14a3ddabf3596aca0332843f6556ff5f7e4a70
unknown <arons18@ru.is>
2019-09-17 11:55:35 +0000

    aaaaaa

--- Actually_kóði.py
+++ Actually_kóði.py
@@ -2,18 +2,25 @@ n=True
 s=True
 w=True
 e=True
-xy=0.0
+pos=1.1
+victory=False
 
-def allowed(xy)
-if xy==1.1 or xy==2.1 or xy== 3.1 or xy==2.2 or xy==3.2 or xy==3.3:
-    e=False
-if xy==1.1 or xy ==2.1 or xy== 3.1 or xy==1.2 or xy==3.2 or xy==1.3:
-    w=False
-if xy==1.1 or xy==2.1 or xy==3.1 or xy==2.3:
-    s=False
-if xy== 1.3 or xy== 2.3 or xy== 3.3 or 2.2:
-    n=False
-return n, e, s, w
+def allowed(xy):
+    n=True
+    s=True
+    w=True
+    e=True
+    if xy==1.1 or xy==2.1 or xy== 3.1 or xy==2.2 or xy==3.2 or xy==3.3:
+        e=False
+    if xy==1.1 or xy ==2.1 or xy== 3.1 or xy==1.2 or xy==3.2 or xy==1.3:
+        w=False
+    if xy==1.1 or xy==2.1 or xy==3.1 or xy==2.3:
+        s=False
+    if xy== 1.3 or xy== 2.3 or xy== 3.3 or xy==2.2:
+        n=False
+    if xy=3.1:
+        victory=True
+    return n, e, s, w
 
 def step(position,command):
     '''Moves position in a 3x3 grid'''
@@ -25,10 +32,10 @@ def step(position,command):
         position += 1.0
     elif command == 'w':
         position -= 1.0
-    return position
+    return round(position,1)
 
 
-def menu(n,s,e,w):
+def menu(n,e,s,w,victory):
     instruction = 'You can travel:'
 
     if n and e and s:
@@ -52,3 +59,16 @@ def menu(n,s,e,w):
     
     elif e:
         option = '{} (E)east'.format(instruction)
+    print(option)
+    return victory
+n,e,s,w = allowed(pos)
+print(allowed(pos))
+print(menu(n,s,e,w))
+print(step(pos,"n"))
+while victory==False:
+    n,e,s,w=allowed(pos)
+    menu(n,e,s,w,victory)
+    command=input("Direction: ")
+    step(pos,command)
+    if command=="n" and n=False:
+        print("Not a valid direction!")
--- README.txt
+++
@@ -1,1 +0,0 @@
-SKJÓTTU MIG
