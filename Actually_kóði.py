65171848a4f301c162ab991c530034648e605bfe
aishareog <aisha9regina@gmail.com>
2019-09-17 11:06:00 +0000

    test

---
+++ Actually_kóði.py
@@ -0,0 +1,36 @@
+
+def step(position,command):
+    '''Moves position in a 3x3 grid'''
+    if command == 'n':
+        position += 0.1
+    elif command == 's':
+        position -= 0.1
+    elif command == 'e':
+        position += 1.0
+    elif command == 'w':
+        position -= 1.0
+    return position
+
+
+def menu(n,s,e,w):
+    instruction = 'You can travel:'
+    directions = 
+    if n and e and s:
+        option = 
+    elif e and s:
+    
+    elif e and w:
+    
+    elif s and w:
+    
+    elif s and e:
+    
+    elif s:
+    
+    elif n:
+    
+    elif e:
+
+     
+        
+        
